0970b927e795212ac916b21f0f6156cf998c4dc5
IngenouslySlow <sunny8188@outlook.com>
2022-12-08 07:32:37 +0000

    02-hello-world.sh

---
+++ 02-hello-world.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# The line above tells shell to use bash as the scripting language
+
+# Echo prints out the line to the terminal when you run the script
+echo "Hiya!"
+
+echo "My current working directory is:"
+# pwd prints the working directory
+pwd
+
+
+
+
